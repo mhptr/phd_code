fdd8fca33ffda466193fb5399d8031b83a81b66f
mhptr_s <swatireturns@gmail.com>
2019-08-23 00:16:48 +0100

    added the regulator percentage for intersect_TP

--- transcriptomics/DESeq2/DESeq2_SM/DEG_regulator_percentage_DW.R
+++ transcriptomics/DESeq2/DESeq2_SM/DEG_regulator_percentage_DW.R
@@ -1,12 +1,12 @@
 #22/08/19
 
-setwd("~/OneDrive - University of Warwick/WORK/Results/Transcriptomic/intersection_toRNAdo_all/")
+setwd("~/OneDrive - University of Warwick/WORK/Results/Trans_Prot_COMBINED/Data/")
 
 # regulation = read.csv("./SubtiWiki Exports /regulations.csv", header = T)
 # regulation_freq = table(regulation$regulator)
-# write.csv(regulation_freq, "./diff.exp.gene/DEG_SM/geneRegulations/regulators/frequency_table/regulation_freq.csv", row.names = F)
+# write.csv(regulation_freq, "./SubtiWiki Exports /regulation_freq.csv", row.names = F)
 
-regulation_freq = read.csv("./diff.exp.gene/DEG_SM/geneRegulations/regulators/frequency_table/regulation_freq.csv", header = T)
+regulation_freq = read.csv("./SubtiWiki Exports /regulation_freq.csv", header = T)
 colnames(regulation_freq) = c("BSU_regulators_all", "No_of_total_targets")
 
 find_ratio = function(regulation_freq, my_table_freq) {
@@ -18,49 +18,48 @@ find_ratio = function(regulation_freq, my_table_freq) {
 }
 
 #DEG_M9
-DEG_M9_geneRegulations = read.csv("./diff.exp.gene/DEG_SM/geneRegulations/DEG_M9_geneRegulations.csv", header = T)
-DEG_M9_reg_freq = table(DEG_M9_geneRegulations$regulator)
-write.csv(DEG_M9_reg_freq, "./diff.exp.gene/DEG_SM/geneRegulations/regulators/percentage_regulator/DEG_M9_reg_freq.csv", row.names = F)
-DEG_M9_reg_freq = read.csv("./diff.exp.gene/DEG_SM/geneRegulations/regulators/percentage_regulator/DEG_M9_reg_freq.csv", header = T)
-colnames(DEG_M9_reg_freq) = c("regulator", "No_of_targets")
-DEG_M9_reg_freq = DEG_M9_reg_freq[!DEG_M9_reg_freq$regulator == "",]
-DEG_M9_reg_percentage = find_ratio(regulation_freq, DEG_M9_reg_freq)
-write.csv(DEG_M9_reg_percentage, "./diff.exp.gene/DEG_SM/geneRegulations/regulators/percentage_regulator/output/DEG_M9_reg_percentage.csv", row.names = FALSE)
-View(DEG_M9_reg_percentage)
+intersect_TP_M9_geneRegulations = read.csv("./Result/geneRegulations/intersect_TP_M9_geneRegulations.csv", header = T)
+intersect_TP_M9_reg_freq = table(intersect_TP_M9_geneRegulations$regulator)
+write.csv(intersect_TP_M9_reg_freq, "./Result/geneRegulations/regulators/percentage_regulator/intersect_TP_M9_reg_freq.csv", row.names = F)
+intersect_TP_M9_reg_freq = read.csv("./Result/geneRegulations/regulators/percentage_regulator/intersect_TP_M9_reg_freq.csv", header = T)
+colnames(intersect_TP_M9_reg_freq) = c("regulator", "No_of_targets")
+intersect_TP_M9_reg_freq = intersect_TP_M9_reg_freq[!intersect_TP_M9_reg_freq$regulator == "",]
+intersect_TP_M9_reg_percentage = find_ratio(regulation_freq, intersect_TP_M9_reg_freq)
+write.csv(intersect_TP_M9_reg_percentage, "./Result/geneRegulations/regulators/percentage_regulator/output/intersect_TP_M9_reg_percentage.csv", row.names = FALSE)
+View(intersect_TP_M9_reg_percentage)
 
 #DEG_SH2
-DEG_SH2_geneRegulations = read.csv("./diff.exp.gene/DEG_SM/geneRegulations/DEG_SH2_geneRegulations.csv", header = T)
-DEG_SH2_reg_freq = table(DEG_SH2_geneRegulations$regulator)
-write.csv(DEG_SH2_reg_freq, "./diff.exp.gene/DEG_SM/geneRegulations/regulators/percentage_regulator/DEG_SH2_reg_freq.csv", row.names = F)
-DEG_SH2_reg_freq = read.csv("./diff.exp.gene/DEG_SM/geneRegulations/regulators/percentage_regulator/DEG_SH2_reg_freq.csv", header = T)
-colnames(DEG_SH2_reg_freq) = c("regulator", "No_of_targets")
-DEG_SH2_reg_freq = DEG_SH2_reg_freq[!DEG_SH2_reg_freq$regulator == "",]
-DEG_SH2_reg_percentage = find_ratio(regulation_freq, DEG_SH2_reg_freq)
-write.csv(DEG_SH2_reg_percentage, "./diff.exp.gene/DEG_SM/geneRegulations/regulators/percentage_regulator/output/DEG_SH2_reg_percentage.csv", row.names = FALSE)
-View(DEG_SH2_reg_percentage)
+intersect_TP_SH2_geneRegulations = read.csv("./Result/geneRegulations/intersect_TP_SH2_geneRegulations.csv", header = T)
+intersect_TP_SH2_reg_freq = table(intersect_TP_SH2_geneRegulations$regulator)
+write.csv(intersect_TP_SH2_reg_freq, "./Result/geneRegulations/regulators/percentage_regulator/intersect_TP_SH2_reg_freq.csv", row.names = F)
+intersect_TP_SH2_reg_freq = read.csv("./Result/geneRegulations/regulators/percentage_regulator/intersect_TP_SH2_reg_freq.csv", header = T)
+colnames(intersect_TP_SH2_reg_freq) = c("regulator", "No_of_targets")
+intersect_TP_SH2_reg_freq = intersect_TP_SH2_reg_freq[!intersect_TP_SH2_reg_freq$regulator == "",]
+intersect_TP_SH2_reg_percentage = find_ratio(regulation_freq, intersect_TP_SH2_reg_freq)
+write.csv(intersect_TP_SH2_reg_percentage, "./Result/geneRegulations/regulators/percentage_regulator/output/intersect_TP_SH2_reg_percentage.csv", row.names = FALSE)
+View(intersect_TP_SH2_reg_percentage)
 
 #DEG_SH5
-DEG_SH5_geneRegulations = read.csv("./diff.exp.gene/DEG_SM/geneRegulations/DEG_SH5_geneRegulations.csv", header = T)
-DEG_SH5_reg_freq = table(DEG_SH5_geneRegulations$regulator)
-write.csv(DEG_SH5_reg_freq, "./diff.exp.gene/DEG_SM/geneRegulations/regulators/percentage_regulator/DEG_SH5_reg_freq.csv", row.names = F)
-DEG_SH5_reg_freq = read.csv("./diff.exp.gene/DEG_SM/geneRegulations/regulators/percentage_regulator/DEG_SH5_reg_freq.csv", header = T)
-colnames(DEG_SH5_reg_freq) = c("regulator", "No_of_targets")
-DEG_SH5_reg_freq = DEG_SH5_reg_freq[!DEG_SH5_reg_freq$regulator == "",]
-DEG_SH5_reg_percentage = find_ratio(regulation_freq, DEG_SH5_reg_freq)
-write.csv(DEG_SH5_reg_percentage, "./diff.exp.gene/DEG_SM/geneRegulations/regulators/percentage_regulator/output/DEG_SH5_reg_percentage.csv", row.names = FALSE)
-View(DEG_SH5_reg_percentage)
+intersect_TP_SH5_geneRegulations = read.csv("./Result/geneRegulations/intersect_TP_SH5_geneRegulations.csv", header = T)
+intersect_TP_SH5_reg_freq = table(intersect_TP_SH5_geneRegulations$regulator)
+write.csv(intersect_TP_SH5_reg_freq, "./Result/geneRegulations/regulators/percentage_regulator/intersect_TP_SH5_reg_freq.csv", row.names = F)
+intersect_TP_SH5_reg_freq = read.csv("./Result/geneRegulations/regulators/percentage_regulator/intersect_TP_SH5_reg_freq.csv", header = T)
+colnames(intersect_TP_SH5_reg_freq) = c("regulator", "No_of_targets")
+intersect_TP_SH5_reg_freq = intersect_TP_SH5_reg_freq[!intersect_TP_SH5_reg_freq$regulator == "",]
+intersect_TP_SH5_reg_percentage = find_ratio(regulation_freq, intersect_TP_SH5_reg_freq)
+write.csv(intersect_TP_SH5_reg_percentage, "./Result/geneRegulations/regulators/percentage_regulator/output/intersect_TP_SH5_reg_percentage.csv", row.names = FALSE)
+View(intersect_TP_SH5_reg_percentage)
 
 #DEG_SH5_vs_SH2
-DEG_SH5_vs_SH2_geneRegulations = read.csv("./diff.exp.gene/DEG_SM/geneRegulations/DEG_SH5_vs_SH2_geneRegulations.csv", header = T)
-DEG_SH5_vs_SH2_reg_freq = table(DEG_SH5_vs_SH2_geneRegulations$regulator)
-write.csv(DEG_SH5_vs_SH2_reg_freq, "./diff.exp.gene/DEG_SM/geneRegulations/regulators/percentage_regulator/DEG_SH5_vs_SH2_reg_freq.csv", row.names = F)
-DEG_SH5_vs_SH2_reg_freq = read.csv("./diff.exp.gene/DEG_SM/geneRegulations/regulators/percentage_regulator/DEG_SH5_vs_SH2_reg_freq.csv", header = T)
-colnames(DEG_SH5_vs_SH2_reg_freq) = c("regulator", "No_of_targets")
-DEG_SH5_vs_SH2_reg_freq = DEG_SH5_vs_SH2_reg_freq[!DEG_SH5_vs_SH2_reg_freq$regulator == "",]
-DEG_SH5_vs_SH2_reg_percentage = find_ratio(regulation_freq, DEG_SH5_vs_SH2_reg_freq)
-write.csv(DEG_SH5_vs_SH2_reg_percentage, "./diff.exp.gene/DEG_SM/geneRegulations/regulators/percentage_regulator/output/DEG_SH5_vs_SH2_reg_percentage.csv", row.names = FALSE)
-View(DEG_SH5_vs_SH2_reg_percentage)
-
+intersect_TP_SH5_vs_SH2_geneRegulations = read.csv("./Result/geneRegulations/intersect_TP_SH5_vs_SH2_geneRegulations.csv", header = T)
+intersect_TP_SH5_vs_SH2_reg_freq = table(intersect_TP_SH5_vs_SH2_geneRegulations$regulator)
+write.csv(intersect_TP_SH5_vs_SH2_reg_freq, "./Result/geneRegulations/regulators/percentage_regulator/intersect_TP_SH5_vs_SH2_reg_freq.csv", row.names = F)
+intersect_TP_SH5_vs_SH2_reg_freq = read.csv("./Result/geneRegulations/regulators/percentage_regulator/intersect_TP_SH5_vs_SH2_reg_freq.csv", header = T)
+colnames(intersect_TP_SH5_vs_SH2_reg_freq) = c("regulator", "No_of_targets")
+intersect_TP_SH5_vs_SH2_reg_freq = intersect_TP_SH5_vs_SH2_reg_freq[!intersect_TP_SH5_vs_SH2_reg_freq$regulator == "",]
+intersect_TP_SH5_vs_SH2_reg_percentage = find_ratio(regulation_freq, intersect_TP_SH5_vs_SH2_reg_freq)
+write.csv(intersect_TP_SH5_vs_SH2_reg_percentage, "./Result/geneRegulations/regulators/percentage_regulator/output/intersect_TP_SH5_vs_SH2_reg_percentage.csv", row.names = FALSE)
+View(intersect_TP_SH5_vs_SH2_reg_percentage)
 
 
 
